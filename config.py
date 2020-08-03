b8e3497bc356807e8b6ac44257e6526a066f8774
tfrizza <tfrizza@gmail.com>
2020-08-03 12:51:08 +1000

    Revamped dataloader as its own self-contained class. Supports preprocessing of trajectories into sub-trajectory chunks and then wrapping that in a tensorflow dataset object. Timeit reckons it takes 340ms to loop through the entire dataset now on average.

---
+++ config.py
@@ -0,0 +1,3 @@
+# Change this to suit your own setup and **don't commit it**
+RPL_PATH = '../relay-policy-learning'
+DEVICE = 'CPU'
--- data.py
+++ data.py
@@ -18,7 +18,7 @@ def build_dataset_from_mjl(base_path='../relay-policy-learning', num_cpus=1):
     # Probably do 1-2 CPUs - using all CPUs will make you computer completely unusable
     pool = ThreadPool(num_cpus) # multiprocessing.cpu_count()
     results = []
-    for path in glob.glob(base_path+'./kitchen_demos_multitask/*'):
+    for path in glob.glob(base_path+'/kitchen_demos_multitask/*'):
         print(path)
         cmd = f'python3 relay-policy-learning/adept_envs/adept_envs/utils/parse_demos.py --env "kitchen_relax-v1" -d "{path}/" -s "40" -v "playback" -r "offscreen"'
         results.append(pool.apply_async(call_proc, (cmd,)))
@@ -41,26 +41,20 @@ def create_single_dataset(base_path='../relay-policy-learning'):
     actions - list of action arrays
     cnt - counter object for plotting distribution of seq lens
     """
-    observations = []
-    actions = []
-    init_poses = []
-    init_vels = []
+    traj_dicts = []
     MAX_SEQ_LEN = 0 # overall max of the dataset is 409 - this is overkill
     cnt = Counter()
 
-    filenames = glob.glob(base_path+'./kitchen_demos_multitask/*/*.pkl')
+    filenames = glob.glob(base_path+'/kitchen_demos_multitask/*/*.pkl')
     random.Random(42).shuffle(filenames) # shuffle the order of trajectory files
 
     for path in filenames:
         with open(path, 'rb') as f:
             traj_dict = pickle.load(f)
-        observations.append(traj_dict['observations'])
-        actions.append(traj_dict['actions'])
-        init_poses.append(traj_dict['init_qpos'])
-        init_vels.append(traj_dict['init_qvel'])
+        traj_dicts.append(traj_dict)
         cnt[len(traj_dict['observations'])]+=1
 
-    return observations, actions, cnt, init_poses, init_vels
+    return traj_dicts, cnt
 
 
 # IO function for reading pickle files - we return as the original dictionary
