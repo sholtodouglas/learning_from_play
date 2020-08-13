3c128fcaed4b87fece14ec41ed65f969ff720cfd
Sholto Douglas <sholto.douglas1@gmail.com>
2020-08-14 04:12:59 +1000

    oops, stuffed that data collection

--- data_collection/full_vr.py
+++ data_collection/full_vr.py
@@ -1,6 +1,8 @@
 # import os
 # os.system(r'"C:\Users\sholt\Desktop\bullet3\bin\App_PhysicsServer_SharedMemory_VR_vs2010_x64_release.exe"')
-USE_VR = None
+
+debugging = False
+
 import socket
 import pybullet as p
 import time
@@ -13,17 +15,6 @@ import gym
 import os
 # p.connect(p.UDP,"192.168.86.100")
 import matplotlib.pyplot as plt
-
-
-# cid = p.connect(p.SHARED_MEMORY)
-#
-# if (cid < 0):
-#     p.connect(p.GUI)
-#     USE_VR = False
-# else:
-#     print("Connected to shared memory")
-#     USE_VR = True  # Use either GUI or VR to set commands.
-# p.resetSimulation()
 
 env= gym.make('pandaPlay-v0')
 env.vr_activation()
@@ -38,7 +29,7 @@ print(pybullet_data.getDataPath())
 
 p.configureDebugVisualizer(p.COV_ENABLE_RENDERING, 0)
 # p.configureDebugVisualizer(p.COV_ENABLE_Y_AXIS_UP , 1)
-p.setVRCameraState([0.0, -0.3, -0.9], p.getQuaternionFromEuler([0, 0, 0]))
+p.setVRCameraState([0.0, -0.3, -0.5], p.getQuaternionFromEuler([0, 0, 0]))
 
 p.configureDebugVisualizer(p.COV_ENABLE_RENDERING, 1)
 p.setRealTimeSimulation(1)
@@ -74,15 +65,11 @@ ORIENTATION = 2
 ANALOG = 3
 BUTTONS = 6
 
-
-
-
-
-
 base_path = 'collected_data/play_demos/'
 obs_act_path = base_path + 'obs_act_etc/'
 env_state_path = base_path + 'states_and_ims/'
-print(obs_act_path)
+
+
 try:
     os.makedirs(obs_act_path)
 except:
@@ -95,20 +82,8 @@ except:
 
 
 
-demo_count = len(list(os.listdir(obs_act_path)))
 
-debugging = False
 
-example_path = env_state_path + str(demo_count)
-npz_path = obs_act_path+str(demo_count)
-if not debugging:
-    os.makedirs(example_path + '/env_states')
-    os.makedirs(example_path + '/env_images')
-    os.makedirs(npz_path)
-counter = 0
-control_frequency = 20 # Hz
-t0 = time.time()
-next_time = t0 + 1/control_frequency
 
 def do_command(t,t0):
     print(t-t0)
@@ -116,7 +91,6 @@ def do_command(t,t0):
     targetPoses = env.panda.goto(POS, ORI, GRIPPER)
     return targetPoses
 
-acts, obs, ags, cagb, joints, targetJoints = [], [], [], [], [], []
 
 
 def save_stuff(env):
@@ -138,7 +112,25 @@ def save(npz_path):
         np.savez(npz_path + '/data', acts=acts, obs=obs, achieved_goals=ags, controllable_achieved_goals=cagb, joint_poses=joints, target_poses=targetJoints)
     print('Finito')
 
-try:
+env.p.saveBullet(os.path.dirname(os.path.abspath(__file__)) + '/init_state.bullet') 
+
+while(1):
+    time.sleep(1)
+    demo_count = len(list(os.listdir(obs_act_path)))
+    example_path = env_state_path + str(demo_count)
+    npz_path = obs_act_path+str(demo_count)
+    if not debugging:
+        os.makedirs(example_path + '/env_states')
+        os.makedirs(example_path + '/env_images')
+        os.makedirs(npz_path)
+    counter = 0
+    control_frequency = 20 # Hz
+    t0 = time.time()
+    next_time = t0 + 1/control_frequency
+    # reset from init
+    env.p.restoreState(fileName = os.path.dirname(os.path.abspath(__file__)) + '/init_state.bullet')
+    
+    acts, obs, ags, cagb, joints, targetJoints = [], [], [], [], [], []
     while(1):
         get_new_command()
         t = time.time()
@@ -155,9 +147,5 @@ try:
             save(npz_path)
             break
 
-
-except:
-    # finito and save
-    save(npz_path)
     
     
