7f4c7d0e66f76939f7fb79c50ef55624db3e7a69
tfrizza <tfrizza@atlassian.com>
2021-02-03 13:49:17 +1100

    Changed matplotlib and plotly version to avoid current bug https://github.com/plotly/plotly.py/issues/2913

--- notebooks/train_lfp.py
+++ notebooks/train_lfp.py
@@ -133,7 +133,7 @@ print("Tensorflow version " + tf.__version__)
 
 if args.device == 'TPU':
     try:
-        tpu = tf.distribute.cluster_resolver.TPUClusterResolver()  # TPU detection
+        tpu = tf.distribute.cluster_resolver.TPUClusterResolver(tpu=args.tpu_name)  # TPU detection
         print('Running on TPU ', tpu.cluster_spec().as_dict()['worker'])
     except ValueError:
         raise BaseException('ERROR: Not connected to a TPU runtime; please see the previous cell in this notebook for instructions!')
