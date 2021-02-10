14f113f2427536665024db87cb14cddac68b9af1
Sholto <sholto.douglas1@gmail.com>
2021-02-11 08:57:18 +1100

    charting the way

--- lfp/utils.py
+++ lfp/utils.py
@@ -14,9 +14,10 @@ def load_weights(path, actor, encoder=None, planner=None, cnn=None, step=""):
         if cnn is not None: # Also load cnn if that is 
             ckpt = tf.train.Checkpoint(step=tf.Variable(1), actor=actor, encoder=encoder, planner=planner, cnn=cnn)
         ckpt.restore(tf.train.latest_checkpoint(path)).expect_partial()
+        print('Checkpoint restored')
     else:
         actor.load_weights(f'{path}/model' + step + '.h5')
         if planner is not None: planner.load_weights(f'{path}/planner' + step + '.h5')
         if encoder is not None: encoder.load_weights(f'{path}/encoder' + step + '.h5')
-        if cnn is not None cnn.load_weights(f'{path}/cnn'+step+'.h5'')
+        if cnn is not None: cnn.load_weights(f'{path}/cnn'+step+'.h5')
 
