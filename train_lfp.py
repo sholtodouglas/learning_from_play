c3af28e224c6c3da4145cfa731090cca074b1a94
Sholto <sholto.douglas1@gmail.com>
2021-05-23 20:45:53 +1000

    updating deploy for new viewpoint

--- train_lfp.py
+++ train_lfp.py
@@ -259,7 +259,7 @@ while t < args.train_steps:
 
         # if not args.images:
             # How we plot the cluster figs
-        batches = [trainer.make_sequences_variable_length(dataset_coordinator.plotting_background_dataset.next()) for i in range(0,2)]
+        batches = [trainer.make_sequences_variable_length(dataset_coordinator.plotting_background_dataset.next()) for i in range(0,1)]
         super_batch = {}
         for k in batches[0].keys():
             super_batch[k] = np.concatenate([b[k] for b in batches])
